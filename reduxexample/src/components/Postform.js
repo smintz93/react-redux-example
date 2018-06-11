ebdf86914021afa491ce60aec88cb6ad1d327aae
Sam <sammintz1993@gmail.com>
2018-06-11 09:40:08 -0500

    grabbed title and body from api call

---
+++ reduxexample/src/components/Postform.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			posts: []
+		}
+	}
+
+	render(){
+		return (
+			<div>
+				<h1>Add</h1>
+			
+			</div>
+		)
+	}
+}
+
+
+export default PostForm; 
