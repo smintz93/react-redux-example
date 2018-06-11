03b8e14be96f5ab47b012aa21725390f2ac569ff
Sam <sammintz1993@gmail.com>
2018-06-11 09:52:21 -0500

    can make post request to api

--- reduxexample/src/components/Postform.js
+++ reduxexample/src/components/Postform.js
@@ -4,14 +4,52 @@ class PostForm extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			posts: []
+			title: "",
+			body: ""
 		}
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onChange = (e) => {
+		this.setState({[e.target.name]: e.target.value })
+	}
+	onSubmit = (e) => {
+		e.preventDefault();
+
+		const post = {
+			title: this.state.title,
+			body: this.state.body
+		}
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(post)
+		})
+		.then(res => res.json())
+		.then(data => console.log(data))
 	}
 
 	render(){
 		return (
 			<div>
-				<h1>Add</h1>
+				<h1>Add Post</h1>
+				<form onSubmit={this.onSubmit}>
+					<div>
+						<label> Title: </label><br />
+						<input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
+					</div>
+					<br />
+					<div>
+						<label> Body: </label><br />
+						<textarea name="body" onChange={this.onChange} value={this.state.body}/>
+						<br />
+						<button type="submit">Submit</button>
+					</div>
+
+				</form>
 			
 			</div>
 		)
